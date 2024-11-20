dd0eafa95f9668de1dd98165ea7f3bdd757992ae
Bermet <iskenderova.bermet19@gmail.com>
2024-11-21 00:44:44 +0600

    React project with Vite was created

---
+++ src/PersonInfo.jsx
@@ -0,0 +1,13 @@
+const PersonInfo = () =>{
+    return(
+        <div>
+            <h1>Name: Bermet Iskenderova </h1>
+            <p>Position: Software developer</p>
+            <p>Company: Development.kg</p>
+            <p>Age: 19 years old</p>
+            <p>Location: Bishkek city/Kyrgyzstan</p>
+            <p>Email: iskenderova.bermet19@gmail.com</p>
+        </div>
+    );
+};
+export default PersonInfo;
